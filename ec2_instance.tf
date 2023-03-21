875b554dd0a1f0653d588eacd78f64a67834c168
GarethProjects <64733108+GarethProjects@users.noreply.github.com>
2023-03-21 17:23:30 +0000

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -21,7 +21,7 @@
 # We have an EC2 instance in AWS that needs to come under Terraform control
 # We do this by creating the AWS resource here.
 
-resource "aws_instance" "my_ec2_instance_imported" {
+resource "aws_instance" "ec2vmfromConsole" {
   ami           = "ami-05502a22127df2492"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0dc9ce31833103bec"
